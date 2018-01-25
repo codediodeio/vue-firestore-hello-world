f8b5d9833ea7b68de7101f42fb8a0ecc5d434807
codediodeio <hello@codediode.io>
2018-01-25 10:32:57 -0700

    full working demo

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <article v-for="(location, idx) in locations" :key="idx">
+      <img :src="location.image" width="300px">
+      <h1>{{ location.name }}</h1>
+      <button class="button is-small is-danger" v-on:click="deleteLocation(location.id)">
+        Delete
+      </button>
+    </article>
+
+    <hr>
+
+    <form v-on:submit="addLocation(name, image)">
+      <h2>Add a New Location</h2>
+      <input v-model="name" placeholder="Location Name" class="input">
+      <input v-model="image" placeholder="Location Image URL" class="input">
+      <button  type="submit" class="button is-success">Add New Location</button>
+    </form>
+  </div>
+</template>
+
+<script>
+
+import { db } from '../main'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      locations: [],
+      name: '',
+      image: ''
+    }
+  },
+  firestore () {
+    return {
+      locations: db.collection('locations').orderBy('createdAt')
+    }
+  },
+  methods: {
+    addLocation (name, image) {
+      const createdAt = new Date()
+      db.collection('locations').add({ name, image, createdAt })
+    },
+    deleteLocation (id) {
+      db.collection('locations').doc(id).delete()
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+input, button {
+  margin-bottom: 10px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,27 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+import VueFire from 'vuefire'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+Vue.use(VueFire)
+
+firebase.initializeApp({
+  projectId: 'YOUR_PROJECT_ID',
+  databaseURL: 'YOUR_DB_URL'
+})
+
+export const db = firebase.firestore()
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
